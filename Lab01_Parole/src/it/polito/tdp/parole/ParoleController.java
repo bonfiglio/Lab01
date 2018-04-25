369e1b18fb6245cdcf1f0b91ad7410cbe962d619
gianl <gianl@192.168.1.6>
2018-04-25 19:07:28 +0200

    parte due punto due completato

--- Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
+++ Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
@@ -15,10 +15,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
+import java.util.*;
 public class ParoleController {
 	
 	Parole elenco = new Parole();
+	List <String> tempi = new ArrayList<String>();
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -40,39 +41,77 @@ public class ParoleController {
     
     @FXML
     private Button btnCanc;
+    @FXML
+    private TextArea txtTime;
 
 
     @FXML
     void doInsert(ActionEvent event) {
     	// TODO
     	
+    	double start = System.nanoTime();
     	String p = txtParola.getText();
     	
     	String result = "";
+    	String tms = "";
     	elenco.addParola(p);
     	txtParola.clear();
-    	
+    	double finish = System.nanoTime();
     	for(String s : elenco.getElenco()) {
-    		result += s +"\n";
+    		result +=s +"\n";
     	}
     	txtResult.setText(result);
+    	
+    	tempi.add("aggiunta:"+(finish-start));
+    	txtTime.clear();
+    	for(String d: tempi) {
+    		tms +=  d+"\n";
+    	}
+    	
+    	txtTime.setText(tms);
     }
     
     @FXML
     void doReset(ActionEvent event) {
     	// TODO
+    	
+    	String tms = "";
+    	double start = System.nanoTime();
     	txtResult.clear();
+    	double finish = System.nanoTime();
+    	
+    	tempi.add("reset:"+(finish-start));
+    	txtTime.clear();
+    	
+    	for(String d: tempi) {
+    		tms += d+"\n";
+    	}
+    	txtTime.setText(tms);
     }
     
     @FXML
     void handleCanc(ActionEvent event) {
-     
+    	
+    	String tms = "";
+    	double start = System.nanoTime();
     	elenco.cancella(txtResult.selectedTextProperty().get());
+    	double finish = System.nanoTime();
+    	
     	String result ="";
     	for(String s : elenco.getElenco()) {
     		result += s +"\n";
     	}
     	txtResult.setText(result);
+    	
+    	tempi.add("cancella:"+(finish-start));
+    	txtTime.clear();
+    	for(String d: tempi) {
+    		tms +=  d+"\n";
+    	}
+    	
+    	txtTime.setText(tms);
+    	
+    	
     }
     
 
@@ -82,7 +121,8 @@ public class ParoleController {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
         assert btnCanc != null : "fx:id=\"btnCanc\" was not injected: check your FXML file 'Parole.fxml'.";
-       
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Parole.fxml'.";
+        
         elenco = new Parole() ;
         
     }
